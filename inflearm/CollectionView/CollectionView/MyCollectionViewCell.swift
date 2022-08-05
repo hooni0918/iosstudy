8fd8e66328045c2d4eaca50d1ce8a9a5520274f1
hooni <jhlee909090@gmail.com>
2022-08-06 01:40:02 +0900

    컬렉션뷰

---
+++ inflearm/CollectionView/CollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionView
+//
+//  Created by 이지훈 on 2022/08/06.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var idolImageView: UIImageView!
+    @IBOutlet weak var labelNAme: UILabel!
+    @IBOutlet weak var labelArt: UILabel!
+
+}
---
+++ inflearm/MYTablelist/MYTablelist/MyTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyTableViewCell.swift
+//  MYTablelist
+//
+//  Created by 이지훈 on 2022/08/05.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var idolImageView: UIImageView!
+    @IBOutlet weak var labelName: UITableView!
+    @IBOutlet weak var labelArt: UITableView!
+    
+    
+}
