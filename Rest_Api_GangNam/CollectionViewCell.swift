6fdec40abcbbb250688b87ce2384b71d281285ce
YIZIHN <109647045+hooni0918@users.noreply.github.com>
2023-01-27 13:10:39 +0900

    0127 connect

---
+++ Rest_Api_GangNam/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  Rest_Api_GangNam
+//
+//  Created by 이지훈 on 2023/01/27.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Rest_Api_GangNam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rest_Api_GangNam
+//
+//  Created by 이지훈 on 2023/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
